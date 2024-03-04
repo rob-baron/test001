f45e93f2ffc0ed97c8abfcfe6cc8a45ecd14332a
rob-baron <robertbartlettbaron@gmail.com>
2024-03-03 19:21:27 -0500

    Added the radar2gis test
    More extensively testing the radar2gis function.

--- p5_CorrdinateTransformation/test/test_overall.cpp
+++ p5_CorrdinateTransformation/test/test_overall.cpp
@@ -30,6 +30,10 @@ namespace
         {{"1S", "179E"}, {"1N", "179W"}, {314.7544702919983, 45}},
         {{"1N", "179W"}, {"1S", "179E"}, {314.7544702919983, 225}},
 
+        /* North is the IDT
+        ** South is the GM
+        ** degrees go clockwise
+        */
         {{"90N", "0E"}, {"89N", "180E"}, {111.20621865770366, 0}},
         {{"90N", "0E"}, {"89N", "135E"}, {111.20621865770366, 45}},
         {{"90N", "0E"}, {"89N", "90E"}, {111.20621865770366, 90}},
@@ -39,14 +43,18 @@ namespace
         {{"90N", "0E"}, {"89N", "90W"}, {111.20621865770366, 270}},
         {{"90N", "0E"}, {"89N", "135W"}, {111.20621865770366, 315}},
 
-        {{"90S", "0E"}, {"89S", "180E"}, {111.20621865770366, 0}},
-        {{"90S", "0E"}, {"89S", "135E"}, {111.20621865770366, 45}},
+        /* defining north is GM
+        ** south is IDL
+        ** degrees go clockwise
+        */
+        {{"90S", "0E"}, {"89S", "180E"}, {111.20621865770366, 180}},
+        {{"90S", "0E"}, {"89S", "135E"}, {111.20621865770366, 135}},
         {{"90S", "0E"}, {"89S", "90E"}, {111.20621865770366, 90}},
-        {{"90S", "0E"}, {"89S", "45E"}, {111.20621865770366, 135}},
-        {{"90S", "0E"}, {"89S", "0E"}, {111.20621865770366, 180}},
-        {{"90S", "0E"}, {"89S", "45W"}, {111.20621865770366, 225}},
+        {{"90S", "0E"}, {"89S", "45E"}, {111.20621865770366, 45}},
+        {{"90S", "0E"}, {"89S", "0E"}, {111.20621865770366, 0}},
+        {{"90S", "0E"}, {"89S", "45W"}, {111.20621865770366, 315}},
         {{"90S", "0E"}, {"89S", "90W"}, {111.20621865770366, 270}},
-        {{"90S", "0E"}, {"89S", "135W"}, {111.20621865770366, 315}},
+        {{"90S", "0E"}, {"89S", "135W"}, {111.20621865770366, 225}},
     };
 
     TEST(gis2radar, overall)
@@ -71,12 +79,18 @@ namespace
         }
     }
 
-    //    TEST(radar2gis, overall)
-    //    {
-    //        location_str_t locstr1, locstr2;
-    //        radar_t radar;
-    //        radar2gis(&locstr1, &radar, &locstr2);
-    //        ASSERT_EQ(0, cmpfp(radar.bearing, 0.0, 0.000001)) << "bearing = " << radar.bearing << std::endl;
-    //        ASSERT_EQ(0, cmpfp(radar.range, 111.20621865770366, 0.000001)) << "range = " << radar.range << std::endl;
-    //    }
+    TEST(radar2gis, overall)
+    {
+        location_str_t locstr1, locstr2;
+        radar_t radar;
+        for (int i = 0; i < num_g2r_tests; i++)
+        {
+            radar2gis(&g2r_data[i].loc_str1, &g2r_data[i].radar, &locstr2);
+            EXPECT_EQ(1, loc_str_isequal(&locstr2, &g2r_data[i].loc_str2)) << i << " location\n"
+                                                                           << "  { " << g2r_data[i].loc_str1.latitude << ", " << g2r_data[i].loc_str1.longitude << "} \n"
+                                                                           << "  { " << g2r_data[i].loc_str2.latitude << ", " << g2r_data[i].loc_str2.longitude << "} \n"
+                                                                           << "  [ " << locstr2.latitude << ", " << locstr2.longitude << " ] \n"
+                                                                           << std::endl;
+        }
+    }
 } // namespace
